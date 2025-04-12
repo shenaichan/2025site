0ef35f5634af9cdac8c02a310100c34984e2b0f1
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-04-12 15:37:24 -0500

    experimenting with new layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,82 @@
-import Image from "next/image";
+import localFont from "next/font/local";
+import "./home.css";
 
-export default function Home() {
+// Font files can be colocated inside of `pages`
+const ergon = localFont({ src: "./Ergon-Light-Trial.otf" });
+const canela = localFont({ src: "./CanelaText-Light-Trial.otf" });
+
+export default function Home({}) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      style={{
+        backgroundColor: "#fff9f0",
+        display: "flex",
+        justifyContent: "center",
+        padding: "40px",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "1000px",
+          maxWidth: "1000px",
+          color: "#201000",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row-reverse",
+          }}
+        >
+          <h1
+            id="me"
+            className={ergon.className}
+            style={{
+              fontWeight: "normal",
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Shenai Chan
+          </h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <br />
+        <div
+          className={canela.className}
+          style={{
+            paddingTop: "20px",
+            paddingRight: "20px",
+            fontSize: "20px",
+            borderTop: "2px solid #201000",
+            borderRight: "2px solid #201000",
+            flexGrow: "1",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div>
+            <p>
+              is a software engineer at{" "}
+              <a
+                style={{ textDecoration: "underline" }}
+                href="https://notion.com"
+              >
+                Notion
+              </a>
+              . They live, observe, and write in lovely, sunny San Francisco.
+            </p>
+            <br></br>
+          </div>
+          <div>
+            <p>
+              <span style={{ fontSize: "24px", marginRight: "8px" }}>✸</span>{" "}
+              GitHub / LinkedIn / email / are.na / blog / old site
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

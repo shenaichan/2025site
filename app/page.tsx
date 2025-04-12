c8bafad7c17cf24bf43e95c2ae62d108057a2de5
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-04-12 16:07:13 -0500

    whatever

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default function Home({}) {
             className={ergon.className}
             style={{
               fontWeight: "normal",
+              // lineHeight: "1",
             }}
           >
             Shenai Chan
@@ -48,8 +49,8 @@ export default function Home({}) {
             paddingTop: "20px",
             paddingRight: "20px",
             fontSize: "20px",
-            borderTop: "2px solid #201000",
-            borderRight: "2px solid #201000",
+            borderTop: "3px solid rgb(177, 44, 146)",
+            borderRight: "3px solid rgb(177, 44, 146)",
             flexGrow: "1",
             display: "flex",
             flexDirection: "column",
@@ -59,20 +60,33 @@ export default function Home({}) {
           <div>
             <p>
               is a software engineer at{" "}
-              <a
-                style={{ textDecoration: "underline" }}
-                href="https://notion.com"
+              <span
+                style={{
+                  textDecoration: "underline",
+                  color: "rgb(177, 44, 146)",
+                  // backgroundColor: "rgb(201, 255, 201)",
+                }}
+                // href="https://notion.com"
               >
                 Notion
-              </a>
+              </span>
               . They live, observe, and write in lovely, sunny San Francisco.
             </p>
-            <br></br>
+            <br />
+            <p>Previously at the Recurse Center and Carnegie Mellon.</p>
           </div>
           <div>
             <p>
-              <span style={{ fontSize: "24px", marginRight: "8px" }}>✸</span>{" "}
-              GitHub / LinkedIn / email / are.na / blog / old site
+              <span
+                style={{
+                  fontSize: "36px",
+                  marginRight: "8px",
+                  color: "rgb(177, 44, 146)",
+                }}
+              >
+                ✸
+              </span>{" "}
+              GitHub / LinkedIn / resume / email / are.na / blog / old site
             </p>
           </div>
         </div>
